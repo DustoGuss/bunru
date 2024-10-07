96cdfc22247f75ecd00fcbd60dbfc788733f2040
DustoGuss <gustavopfeffer29@gmail.com>
2024-10-07 09:24:18 -0300

    BUNRU

--- src/BunruInstaller.cpp
+++ src/BunruInstaller.cpp
@@ -5,39 +5,92 @@
 
 namespace fs = std::filesystem;
 
+fs::path current_path = fs::current_path();
+fs::path bunru_path = current_path / "bin/bunru";
+fs::path target_path = "/usr/local/bin/bunru";
+
+bool reinstalling = false;
+bool alreadyinstalled()
+{
+    if (fs::exists(target_path)) 
+    {
+        return true;
+    }
+    else if (!fs::exists(target_path))
+    {
+        return false;
+    }
+}
+
+void InstallBunru()
+{
+    if (alreadyinstalled() == true) 
+    {
+        std::cerr << "Error: BUNRU is already installed at " << target_path << "." << std::endl;
+    }
+
+    try 
+    {
+        fs::copy(bunru_path, target_path);
+        if (reinstalling == false)
+        {
+            std::cout << "BUNRU installed successfully! You can now close the installer." << std::endl;
+        }
+    }
+    catch (const fs::filesystem_error& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+}
+
+void UninstallBunru()
+{
+    if (alreadyinstalled() == true)
+    {
+        fs::remove_all(target_path);
+    } 
+    else if (alreadyinstalled() == false)
+    {
+        std::cerr << "You don't have BUNRU installed, please reopen the installer, if the error continues, redownload/download the latest release from the Github Repository (https://github.com/DustoGuss/bunru)" << std::endl;
+    }
+}
+
 int main()
 {
     char proceed;
-    std::cout << "Welcome to the BUNRU Installer! Proceed with the installation? (y/n) ";
-    std::cin >> proceed;
-
-    if (proceed == 'y' || proceed == 'Y') 
+    alreadyinstalled();
+    if (alreadyinstalled() == false) 
     {
-        fs::path current_path = fs::current_path();
-        fs::path bunru_path = current_path / "bin/bunru";
-        fs::path target_path = "/usr/local/bin/bunru";
+        std::cout << "Welcome to the BUNRU Installer! Proceed with the installation? (y/n) ";
+        std::cin >> proceed;
 
-        if (fs::exists(target_path)) 
+        if (proceed == 'y' || proceed == 'Y') 
         {
-            std::cerr << "Error: BUNRU is already installed at " << target_path << "." << std::endl;
-            return 1;
-        }
-
-        try 
+            InstallBunru();
+        } 
+        else 
         {
-            fs::copy(bunru_path, target_path);
-            std::cout << "BUNRU installed successfully! You can now close the installer." << std::endl;
-        }
-        catch (const fs::filesystem_error& e)
-        {
-            std::cerr << "Error: " << e.what() << std::endl;
-            return 1;
+            std::cout << "\nInstallation canceled. Goodbye! :D" << std::endl;
         }
     } 
     else 
     {
-        std::cout << "Installation canceled. Goodbye!" << std::endl;
-    }
+        std::cout << "Welcome to the BUNRU Installer! It seems like you already installed BUNRU, do you want to reinstall or uninstall BUNRU? (r: reinstall/u: uninstall): ";
+        std::cin >> proceed;
 
+        if (proceed == 'r' || proceed == 'R')
+        {
+            std::cout << "\nReinstalling...\n" << std::endl;
+            reinstalling = true;
+            UninstallBunru();
+            InstallBunru();
+            std::cout << "BUNRU succesfully reinstalled! :D" << std::endl;
+        } else if (proceed == 'u' || proceed == 'U')
+        {
+            std::cout << "\nUninstalling BUNRU...\n" << std::endl;
+            UninstallBunru();
+            std::cout << "BUNRU succesfully uninstalled! :D" << std::endl;
+        }
+    }
     return 0;
 }
